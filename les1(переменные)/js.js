d9afea03f6a9207bd541e6bc8d76fc0a0ef6afe1
Vlad <bebe4kaxd@gmail.com>
2020-05-15 20:53:02 +0300

    add less folder

---
+++ les1(переменные)/js.js
@@ -0,0 +1,26 @@
+
+
+if (num < 49){
+    console.log('ne verno');
+}else if (num > 100) {
+    console.log('bplshe 100');
+}else{
+    console.log('30');
+}
+(num > 49) ? console.log('verno') : console.log('ne verno');
+
+let num = 50;
+switch (num) {
+    case num < 49: 
+        console.log('ne verno');
+    break;
+    case num > 100:
+        console.log('bolse 100');
+    break;
+    case num > 80:
+        console.log('mnogo');
+    break;
+    case 50:
+        console.log('= 50');
+    break;
+}
