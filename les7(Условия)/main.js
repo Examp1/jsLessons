18abe454ca7a1b3e0fb8fb72b72eab18b9529a69
Vlad <bebe4kaxd@gmail.com>
2020-05-17 23:33:49 +0300

    complete less 7

---
+++ les7(Условия)/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// if (2 == 3) {
+//     console.log("ok");
+// } else {
+//     console.log("nah");
+// }
+
+const num = 5;
+
+// if ( num < 49 ) {
+//     console.log('error');
+// } else if (num == 50) {
+//     console.log('true');
+// } else if (num <= 100) {
+//  console.log('so match');
+// } else {
+//  console.log('> 100');
+// }
+
+// (num === 50) ? console.log('true') : console.log('false');
+
+
+switch (num) {
+    case 49:
+        console.log('Neverno');
+    break;
+    case 100:
+        console.log('100');
+    break;
+    case 50:
+        console.log('50');
+    break;
+    default: 
+     console.log('default');
+    break;
+}
